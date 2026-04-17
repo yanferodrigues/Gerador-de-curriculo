2b475baf08130b64cb75f397efb045a270e20f56
Yan felipe <yanfelipe678@gmail.com>
2026-04-17 14:18:25 -0300

    ESCRITÓRIO 17/04/2026 14:20

--- setup/settings.py
+++ setup/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.211.100.93', "localhost"]
+ALLOWED_HOSTS = ['10.211.100.93', '127.0.0.1', "localhost"]
 
 
 # Application definition
